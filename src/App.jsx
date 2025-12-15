ba7589c08a4eb8a3c7ce1c220d03d27647410134
amabella-aguiluz <amabellaaguiluz1@gmail.com>
2025-12-15 10:28:24 +0800

    added button components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import wide_pic from "./assets/wide.jpg"
 import portrait_pic from "./assets/pfp.jpg"
+import { DiGithubBadge } from "react-icons/di";
 
 function HeaderText({header}){
   return(
@@ -34,15 +35,41 @@ function PortraitImage({src, alt}){
     <div className="flex w-auto h-auto">
       <img src={src} alt={alt} className="w-auto h-full"/>
     </div>
+  )}
+
+
+function ButtonIcon({text, onClick, icon}){
+  return(
+    <button onClick={onClick} className="px-4 py-2 bg-blue-500 text-white rounded flex items-center gap-2">
+      {icon}{text}
+    </button>
+  )
+}
+
+function ButtonIconLink({text,url, icon}){
+  const click= () => {
+  window.open(url, "_blank");
+  };
+  return(
+    <button onClick={click} className="px-4 py-2 bg-gray-800 text-white rounded flex items-center gap-2 hover:bg-gray-700">
+      {icon}{text}
+    </button>
   )
 }
 
 
+function Button({text, onClick}){
+  return(
+    <button onClick={onClick} className="px-1 py-1 bg-blue-500 text-white rounded">
+      {text}
+    </button>
+  )
+}
 
 // assets: 
-//   text box !
-//   wide image !
-//   portrait image !
+//   !text box 
+//   !wide image 
+//   !portrait image 
 //   text box (front) + wide image 
 
 // about me:
@@ -70,6 +97,8 @@ function App() {
       <RegularText text="test" />
       {/* <WideImage src={wide_pic} alt='wide image'/>
       <PortraitImage src={portrait_pic} alt='portrait image'/> */}
+      <ButtonIcon text="Click Me" onClick={() => alert("Button Clicked!")} icon={ <DiGithubBadge size="1.5em"/> } />
+      <ButtonIconLink text="GitHub" url="https://github.com/amabella-aguiluz" icon={ <DiGithubBadge size="1.5em"/> } />
     </>
   )
 }
