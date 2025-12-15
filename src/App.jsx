83f028a43490d7c649ea04734807b7dbe112bf72
amabella-aguiluz <amabellaaguiluz1@gmail.com>
2025-12-15 10:59:42 +0800

    successfully added a skillset icon asset

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,20 @@ import wide_pic from "./assets/wide.jpg"
 import portrait_pic from "./assets/pfp.jpg"
 import { DiGithubBadge } from "react-icons/di";
 
+// skillset icons
+import { DiGit } from "react-icons/di";
+import { DiJavascript } from "react-icons/di";
+import { DiPython } from "react-icons/di";
+import { DiReact } from "react-icons/di";
+
+
+
+const colors = {
+    primary: "#007bff",
+    secondary: "#6c757d",
+    success: "#28a745",
+    accent: "#ff5722",};
+
 function HeaderText({header}){
   return(
     <div className="w-auto h-auto text-3xl font-bold">
@@ -57,13 +71,21 @@ function ButtonIconLink({text,url, icon}){
   )
 }
 
-
 function Button({text, onClick}){
   return(
     <button onClick={onClick} className="px-1 py-1 bg-blue-500 text-white rounded">
       {text}
     </button>
   )
+}
+
+function SkillIcon({icon, text}){
+  return(
+    <div className="flex flex-col items-center bg-grey p-4 rounded shadow w-auto max-w-[100px] h-auto max-h-[120px]">
+      {icon}
+      <div className="text-sm mt-2">{text}</div>
+    </div>
+  );
 }
 
 // assets: 
@@ -73,14 +95,11 @@ function Button({text, onClick}){
 //   text box (front) + wide image 
 
 // about me:
-  // portrait
-  // text
+  // portrait + text
   // about me - bio
   // experience
 
-// skillset:
-  // icon
-  // text box
+// !skillset
 
 // portfolio:
   // title
@@ -99,6 +118,7 @@ function App() {
       <PortraitImage src={portrait_pic} alt='portrait image'/> */}
       <ButtonIcon text="Click Me" onClick={() => alert("Button Clicked!")} icon={ <DiGithubBadge size="1.5em"/> } />
       <ButtonIconLink text="GitHub" url="https://github.com/amabella-aguiluz" icon={ <DiGithubBadge size="1.5em"/> } />
+      <SkillIcon icon={ <DiJavascript size="2em" /> } text="JavaScript"/>
     </>
   )
 }
