c0fc56e8672632e71aaadfa2d64da8c82134c1c9
amabella-aguiluz <amabellaaguiluz1@gmail.com>
2025-12-16 14:33:06 +0800

    undefined serviceid

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
- const serviceId = service_10v1tjq
+ const serviceId = service_10v1tjq;
  const templateId = template_omfro4h;
  const publicKey = k4gzPCvIIUGBXUTZJ;
 
