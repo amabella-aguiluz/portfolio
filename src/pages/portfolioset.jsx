821fc336c8e0256e62d64010e85176be778b1b37
amabella-aguiluz <amabellaaguiluz1@gmail.com>
2025-12-17 13:19:04 +0800

    Fixed spacing on the 'contact me' form

--- src/pages/portfolioset.jsx
+++ src/pages/portfolioset.jsx
@@ -4,23 +4,23 @@ const PortfolioSet = ({id}) => {
   return(
     <div id={id} className="divBg">
           <h2> Projects </h2>
-          <br />
+    <br />
     <div className="flex flex-row gap-2 flex-wrap">
       <PortfolioSample title="Spinagotchi"
         skills="React, TailwindCSS, ElectronJS" 
         description="A desktop tamagotchi."
         githubUrl="https://github.com/amabella-aguiluz/spinagotchi" />
-        <br />
+    <br />
       <PortfolioSample title="Angela_Bot"
           skills="Python, SQLite"
           description="A general-use Discord bot."
           githubUrl="https://github.com/amabella-aguiluz/angela-bot" />
-          <br />
+      <br />
       <PortfolioSample title="GigBytes"
         skills="Figma"
         description="A UI/UX mockup for an app where you can find local gigs anywhere."
         githubUrl="https://github.com/amabella-aguiluz/gigbytes_demo" />
-        <br />
+      <br />
       <PortfolioSample title="Portfolio Site"
         skills="React, TailwindCSS"
         description="You're here! My personal portfolio website."
