88e71c3bb10d4d1f54963e57c2de699139e7bb01
amabella-aguiluz <amabellaaguiluz1@gmail.com>
2025-12-18 09:28:54 +0800

    Made the contacts page scroll down with you on desktop

--- src/pages/contactme.jsx
+++ src/pages/contactme.jsx
@@ -3,7 +3,7 @@ import ContactLinks from "./contactlinks.jsx";
 
 const ContactMe = ({id}) => {
   return(
-  <div id={id} className="flexcol w-full h-auto gap-4 divBg">
+  <div id={id} className="flexcol sticky w-full h-auto gap-4 divBg">
     <div className="flex flex-row justify-between items-center gap-5"><h2>Contact Me</h2> <ContactLinks className="self-end"/></div>
 <div><ContactUs /></div>
 </div>
